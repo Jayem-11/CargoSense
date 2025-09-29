dd2f3a886a2b5c4cd72aa135c99e7cd3d52df2bf
Jayem-11 <ronojohnmichael@gmail.com>
2025-09-29 11:54:23 +0300

    riskagent_update

--- cargosense_orchestrator/main.py
+++ cargosense_orchestrator/main.py
@@ -10,7 +10,8 @@ BASES = {
     "weather": "https://maestro-0367185f-c6c6-43bb-9315-12cc4b17d9a8-zcaxlbuauq-uc.a.run.app/enrich_weather",
     "traffic": "https://maestro-18d44df7-8b0d-44e2-8322-a74cfc7876cc-zcaxlbuauq-uc.a.run.app/enrich_congestion",
     "features": "https://maestro-bd5db7bb-ed52-42e7-81da-e069b695b160-zcaxlbuauq-uc.a.run.app/build_features",
-    "risk": "https://maestro-6335e4e6-a469-45e7-9764-e5f7928b88e2-zcaxlbuauq-uc.a.run.app/score",
+    # "risk": "https://maestro-6335e4e6-a469-45e7-9764-e5f7928b88e2-zcaxlbuauq-uc.a.run.app/score",
+    "risk": "https://maestro-14b52318-f86b-4ff1-9a83-82edfc4215cf-zcaxlbuauq-uc.a.run.app/score",
     "explain": "https://maestro-05e7beeb-2a1a-49d9-bfe2-5eb7799c91aa-zcaxlbuauq-uc.a.run.app/explain",
     "notify": "https://maestro-8881cf6e-c07b-4bce-98a3-34318e42cdfd-zcaxlbuauq-uc.a.run.app/notify",
 }
@@ -67,7 +68,6 @@ def health():
 @app.post("/cargosense")
 async def notify_endpoint(request: Request):
     shipment = await request.json()
-
     # Ensure it's a list
     if not isinstance(shipment, list):
         raise HTTPException(status_code=400, detail="Input must be a list of shipments")
